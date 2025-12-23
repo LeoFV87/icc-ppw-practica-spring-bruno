3d434b783dc99c6999e5df3b54c6c922a4d7e6ac
Boris <bpuente@gmail>
2025-12-22 23:21:52 -0500

    ultimos cambios

--- src/main/java/ec/edu/ups/icc/fundamentos01/products/services/ProductsService.java
+++ src/main/java/ec/edu/ups/icc/fundamentos01/products/services/ProductsService.java
@@ -1,5 +1,18 @@
 package ec.edu.ups.icc.fundamentos01.products.services;
 
-public class ProductsService {
+import java.util.List;
+
+import ec.edu.ups.icc.fundamentos01.products.dtos.CreateProductsDto;
+import ec.edu.ups.icc.fundamentos01.products.dtos.PartialUpdateProductsDto;
+import ec.edu.ups.icc.fundamentos01.products.dtos.ProductsResponseDto;
+
+public interface ProductsService {
     
+   List<ProductsResponseDto> findAll();
+    Object findOne(int id);
+    ProductsResponseDto create(CreateProductsDto dto);
+    Object partialUpdate(int id, PartialUpdateProductsDto dto);
+    Object delete(int id);
+
+
 }
